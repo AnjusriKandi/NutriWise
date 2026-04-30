685f4e90b486816dedcd6491db11d2807a39aac3
Anjusri Kandi <anjusrikandi@gmail.com>
2026-04-30 17:21:43 +0530

    google callback url update in passport

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://nutriwise-cmpb.onrender.com/api/auth/google/callback",
     },
     async (_, __, profile, done) => {
       let user = await User.findOne({ email: profile.emails[0].value });
